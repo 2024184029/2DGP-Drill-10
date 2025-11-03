bf3b28459a2ae33f5fb300d29a2cb8fc9d7be241
정유리 <2024184029@tukorea.ac.kr>
2025-11-03 16:46:52 +0900

    새 뗴가 좌우로 반복 비행하게 함

--- bird.py
+++ bird.py
@@ -11,25 +11,43 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAME_PER_ACTION = 8
 
-# Bird Run Speed
+# Bird Fly Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+FLY_SPEED_KMPH  = 20.0
+FLY_SPEED_MPM   = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+FLY_SPEED_MPS   = (FLY_SPEED_MPM / 60.0)
+FLY_SPEED_PPS   = (FLY_SPEED_MPS * PIXEL_PER_METER)
 
 class Bird:
-    def __init__(self):
+    def __init__(self, boundary_left = 100, boundary_right = 1500):
 
-        self.item = None
-
+        self.frames = 5
         self.x, self.y = 400, 90
         self.frame = 0
         self.face_dir = 1
-        self.dir = 0
+        self.dir = 1
         self.image = load_image('bird_animation.png')
 
+        self.boundary_left, self.boundary_right = boundary_left, boundary_right
+
     def update(self):
+        self.frame = (self.frame + FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % self.frames
+
+        self.x += self.dir * FLY_SPEED_PPS * game_framework.frame_time
+        if self.x > self.boundary_right:
+            self.x = self.boundary_right
+            self.dir = self.face_dir = -1
+        elif self.x < self.boundary_left:
+            self.x = self.boundary_left
+            self.dir = self.face_dir = 1
         pass
 
 
     def draw(self):
+        if self.face_dir == 1:  # 오른쪽
+            self.image.clip_draw(int(self.frame) * 180, 170, 180, 170, self.x, self.y)
+        else:
+            self.image.clip_composite_draw(int(self.frame) * 180, 170, 180, 170, 0, 'h', self.x, self.y, 180, 170)
         pass
 
 
--- play_mode.py
+++ play_mode.py
@@ -2,11 +2,12 @@ from pico2d import *
 
 from boy import Boy
 from grass import Grass
+from bird import Bird
 import game_world
 
 import game_framework
 
-
+bird = None
 boy = None
 
 def handle_events():
@@ -18,19 +19,24 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        else:
-            boy.handle_event(event)
+        # else:
+        #     boy.handle_event(event)
 
 def init():
     global boy
+    global bird
     global running
 
     running = True
     grass = Grass()
     game_world.add_object(grass, 0)
 
-    boy = Boy()
-    game_world.add_object(boy, 1)
+    # boy = Boy()
+    # game_world.add_object(boy, 1)
+
+    birds = [Bird() for i in range(10)]
+    for bird in birds:
+        game_world.add_object(bird, 1)
 
 def update():
     game_world.update()
